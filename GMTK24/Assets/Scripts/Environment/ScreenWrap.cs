7468353044cdaa0e5fcdb778ae6d604b2f2faaec
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-08-17 21:06:21 -0700

    add enemy spawner

--- GMTK24/Assets/Scripts/Environment/ScreenWrap.cs
+++ GMTK24/Assets/Scripts/Environment/ScreenWrap.cs
@@ -10,35 +10,35 @@ public class ScreenWrap : MonoBehaviour
     private float innerOffset;
     BoxCollider2D bc;
 
-    private float xMax = 0;
-    private float xMin = 0;
-    private float yMin = 0;
-    private float yMax = 1;
+    public float XMax { get; private set; } = 0;
+    public float XMin { get; private set; } = 0;
+    public float YMin { get; private set; } = 0;
+    public float YMax { get; private set; } = 1;
 
     private void Awake() {
         bc = GetComponent<BoxCollider2D>();
         bc.size = boundsDimension;
-        xMin = -bc.size.x / 2;
-        xMax = bc.size.x / 2;
-        yMin = -bc.size.y / 2;
-        yMax = bc.size.y / 2;
+        XMin = -bc.size.x / 2;
+        XMax = bc.size.x / 2;
+        YMin = -bc.size.y / 2;
+        YMax = bc.size.y / 2;
     }
 
     private void OnTriggerExit2D(Collider2D other) {
         // Debug.Log(other.name +  " has left the border");
         Vector3 objPos = other.transform.position;
         Vector2 newPos = objPos;
-        if (objPos.x > xMax) {
-            newPos.x = xMin + innerOffset;
+        if (objPos.x > XMax) {
+            newPos.x = XMin + innerOffset;
         }
-        if (objPos.x < xMin) {
-            newPos.x = xMax - innerOffset;
+        if (objPos.x < XMin) {
+            newPos.x = XMax - innerOffset;
         }
-        if (objPos.y > yMax) {
-            newPos.y = yMin + innerOffset;
+        if (objPos.y > YMax) {
+            newPos.y = YMin + innerOffset;
         }
-        if (objPos.y < yMin) {
-            newPos.y = yMax - innerOffset;
+        if (objPos.y < YMin) {
+            newPos.y = YMax - innerOffset;
         }
         // Debug.Log("Tp object to: " + newPos);
         other.transform.position = newPos;
--- GMTK24/Assets/Scripts/Utility/Spawner.cs
+++ GMTK24/Assets/Scripts/Utility/Spawner.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ObjectPooler))]
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField]
+    private float spawnCooldown = 1f;
+    
+    [SerializeField]
+    [Tooltip("Will spawn objects within this radius")]
+    private float spawnOuterRadius;
+    [SerializeField]
+    [Tooltip("Will NOT spawn objects within this radius")]
+    private float spawnInnerRadius;
+
+    private float timer = 0;
+    private ObjectPooler objectPooler;
+    private Transform playerTransform;
+
+    private void Awake() {
+        objectPooler = GetComponent<ObjectPooler>();
+        List<Transform> tempList = new();
+        playerTransform = GameObject.FindWithTag("Player").transform;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (timer <= 0f) {
+            SpawnObject();
+            timer = spawnCooldown;
+        }
+        timer -= Time.deltaTime;
+    }
+
+    void SpawnObject(){
+        GameObject obj = objectPooler.GetPooledObject(); 
+        if (obj != null) {
+            Vector2 spawnPos;
+            do {
+                spawnPos = Random.insideUnitCircle.normalized;
+            } while ( spawnPos == Vector2.zero);
+            spawnPos *= Random.Range(spawnInnerRadius, spawnOuterRadius);
+            obj.transform.position = spawnPos + (Vector2)playerTransform.position;
+            obj.SetActive(true);
+        }
     }
 }
